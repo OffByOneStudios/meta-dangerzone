b4cb90d79378389f08f343aa7ff8b25ea4fcb727
Clark Rinker <cgrinker@gmail.com>
2014-04-22 02:11:27 -0700

    Adding Empty Python plugin.

---
+++ plugins/metadangerzone/template/python/empty[0.1.0]/__plugin__.py
@@ -0,0 +1,15 @@
+from madz.plugin_stub import *
+
+plugin = Plugin(
+    name="metadangerzone.template.python.empty",
+    implementation="interface",
+
+    language="python",
+
+    depends=[
+    ],
+    imports=[
+    ],
+    
+    description=MdlFileLoader("plugin.mdl"),
+)
